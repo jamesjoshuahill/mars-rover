abae4b4607bd4a0d561f5f6c0763717e8ce1ae0b
Allessandro De Simone and Josh Hill <alexdesi+jamesjoshuahill@gmail.com>
2016-02-03 19:30:29 +0000

    test refactor

--- spec/rover_position_spec.rb
+++ spec/rover_position_spec.rb
@@ -1,20 +1,18 @@
 require 'rover_position'
 
 RSpec.describe RoverPosition do
-   it 'returns a default position' do
-     rp = RoverPosition.new
 
+  let(:rp) { RoverPosition.new }
+   it 'returns a default position' do
      expect(rp.position).to eq([0,0,:n])
    end
 
    it '#l' do
-     rp = RoverPosition.new
      rp.l
      expect(rp.position).to eq([0,0,:w])
    end
 
     it '#r' do
-     rp = RoverPosition.new
      rp.r
      expect(rp.position).to eq([0,0,:e])
    end
