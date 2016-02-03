5490b2562263872db397f0462bfa30a9e578a259
Allessandro De Simone and Josh Hill <alexdesi+jamesjoshuahill@gmail.com>
2016-02-03 19:25:54 +0000

    refactor direction

--- lib/rover_position.rb
+++ lib/rover_position.rb
@@ -1,6 +1,7 @@
 class RoverPosition
   attr_reader :position
   def initialize
+    @direction_index = 0
     @position = [0, 0, :n]
   end
 
@@ -8,4 +9,10 @@ class RoverPosition
     @position = [0, 0, :w]
   end
 
+  private
+  DIRECTIONS = [:n, :e, :s, :w]
+
+  def direction
+    DIRECTIONS[@direction_index % 4]
+  end
 end
